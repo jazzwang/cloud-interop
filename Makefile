d6f63092d77a91c0a0db51a0937cf1213ce6c168
Jazz Wang <jazz@vmm.(none)>
2013-02-26 23:32:11 +0800

    * modified Makefile    - add one-templates folder and download vm template from offical      website.  * modified preseed/one-postinst.sh    - add onevnet and onevm create test

--- Makefile
+++ Makefile
@@ -1,13 +1,15 @@
 ISO_URL  := http://ftp.twaren.net/ubuntu-cd/precise/
 ISO_FILE := ubuntu-12.04.2-alternate-amd64.iso
+ONE_TMPL := ttylinux.tar.gz
 
 all: iso
 
 iso: 
-	mkdir -p cd-src cd-dst
+	mkdir -p cd-src cd-dst one-templates
 	if [ ! -f /usr/bin/genisoimage ]; then apt-get -y install genisoimage; fi
 	if [ ! -f /usr/bin/isohybrid ]; then apt-get -y install syslinux; fi
-	if [ ! -f $(ISO_FILE) ]; then wget $(ISO_URL)/$(ISO_FILE); fi
+	if [ ! -f "$(ISO_FILE)" ]; then wget $(ISO_URL)/$(ISO_FILE); fi
+	if [ ! -f "one-templates/$(ONE_TMPL)" ]; then wget http://dev.opennebula.org/attachments/download/170/$(ONE_TMPL) -O one-templates/$(ONE_TMPL); fi
 	mount -o loop $(ISO_FILE) cd-src/
 	rsync -av cd-src/ cd-dst/
 	umount cd-src
--- preseed/one-postinst.sh
+++ preseed/one-postinst.sh
@@ -40,3 +40,76 @@ TM_MAD = [
     arguments  = "tm_shared/tm_shared.conf" ]
 EOF
 su - oneadmin -c "one stop; one start; onehost create localhost im_kvm vmm_kvm tm_ssh dummy"
+
+## Sample setup
+## -----------------------------------------
+## Here is a sample setup which is adapted from
+## <http://www.opennebula.org/documentation:rel3.2:vmg>
+## It's based on a simple and small image of TTYLinux <http://minimalinux.org/ttylinux/>
+## Download a tarball from OpenNebula project :
+##   oneadmin@controller> cd
+##   oneadmin@controller> mkdir one-templates
+##   oneadmin@controller> cd one-templates
+##   oneadmin@controller> wget http://dev.opennebula.org/attachments/download/170/ttylinux.tar.gz
+##   oneadmin@controller> tar xvzf ttylinux.tar.gz
+
+mkdir -p /var/lib/one/one-templates
+tar zxvf /cdrom/one-templates/ttylinux.tar.gz -C /var/lib/one/one-templates
+
+## Then, create a first virtual network (using virbr0 as bridge by default) :
+##   oneadmin@controller> vi small_network.net
+##  -------------------
+##  NAME = "Small network"
+##  TYPE = FIXED
+##  # virbr0 = bridge device used by libvirt /etc/libvirt/qemu/networks/default.xml
+##  BRIDGE = virbr0
+##  LEASES = [ IP="192.168.122.2"]
+##  -------------------
+##    oneadmin@controller> onevnet create small_network.net
+##    oneadmin@controller> onevnet list
+##    ID USER     NAME              TYPE BRIDGE P #LEASES
+##     0 oneadmin Small network    Fixed virbr0 N       0
+
+if [ ! -f /var/lib/one/small_network.net ]; then
+  cat >> /var/lib/one/small_network.net << EOF
+NAME = "Small network"
+TYPE = FIXED
+BRIDGE = virbr0
+LEASES = [ IP="192.168.122.2"]
+EOF
+fi
+
+su - oneadmin -c "onevnet create small_network.net"
+
+## Finally, you can create the template configuration for this small VM :
+## 
+##   oneadmin@controller> vi ttylinux.one
+## -------------------
+## NAME   = ttylinux
+## CPU    = 0.1
+## MEMORY = 64
+## DISK   = [
+##   source   = "/var/lib/one/one-templates/ttylinux.img",
+##   target   = "hda",
+##   readonly = "no" ]
+## NIC    = [ NETWORK = "Small network" ]
+## -------------------
+##   oneadmin@controller> onevm create ttylinux.one
+
+if [ ! -f /var/lib/one/ttylinux.one ]; then
+  cat >> /var/lib/one/ttylinux.one << EOF
+NAME   = ttylinux
+CPU    = 0.1
+MEMORY = 64
+DISK   = [
+  source   = "/var/lib/one/one-templates/ttylinux.img",
+  target   = "hda",
+  readonly = "no" ]
+NIC    = [ NETWORK = "Small network" ]
+EOF
+fi
+
+su - oneadmin -c "onevm create ttylinux.one"
+
+
+
