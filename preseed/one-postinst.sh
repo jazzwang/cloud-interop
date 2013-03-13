46f1457fb604d4cf12f42352a0c94f0d275d4442
Jazz Wang <jazzwang.tw@gmail.com>
2013-03-13 08:40:31 +0800

    * tiny fix: /usr/lib/one/ruby/cloud/occi/occi-server.rb should be executable.
     one@vm:~$ sudo occi-server start
     Can not find /usr/lib/one/ruby/cloud/occi/occi-server.rb.
    
    one@vm:~$ sudo bash -x occi-server start
    + '[' -z '' ']'
    + OCCI_PID=/var/run/one/occi-server.pid
    + OCCI_SERVER=/usr/lib/one/ruby/cloud/occi/occi-server.rb
    + OCCI_LOCK_FILE=/var/lock/one/.occi.lock
    + OCCI_LOG=/var/log/one/occi-server.log
    + OCCI_ETC=/etc/one/occi-server.conf
    + case "$1" in
    + setup
    ++ grep '^IMAGE_DIR=' /etc/one/occi-server.conf
    + eval
    + export TMPDIR=/tmp
    + TMPDIR=/tmp
    + mkdir -p /tmp
    + '[' -f /var/lock/one/.occi.lock ']'
    + start
    + '[' '!' -x /usr/lib/one/ruby/cloud/occi/occi-server.rb ']'
    + echo 'Can not find /usr/lib/one/ruby/cloud/occi/occi-server.rb.'
    Can not find /usr/lib/one/ruby/cloud/occi/occi-server.rb.
    + exit 1

--- preseed/one-postinst.sh
+++ preseed/one-postinst.sh
@@ -123,6 +123,9 @@ NIC    = [ NETWORK = "Small network" ]
 EOF
 fi
 
+## Enable OCCI Server 
+## FIXME: there is a bug of opennebula 3.2.1-2 package
+chmod a+x /usr/lib/one/ruby/cloud/occi/occi-server.rb
 ## -----------------------------
 ## Ref: https://github.com/gwdg/rOCCI-server
 ## -----------------------------
